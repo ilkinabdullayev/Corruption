2921ed82f46cb55814ff2eadb84e02185e39f12a
Administrator <Administrator@Ilkin-PC>
2015-04-29 14:43:14 +0400

    init

--- src/corruption/Main.java
+++ src/corruption/Main.java
@@ -5,10 +5,9 @@
  */
 package corruption;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -26,27 +25,21 @@ public class Main {
         int count = scanner.nextInt();
         int percent = scanner.nextInt();
 
-        List<Double> listAccount = new ArrayList<Double>();
+        Queue<Double> listAccount = new PriorityQueue<Double>();
         for (int i = 0; i < count; i++) {
             listAccount.add(scanner.nextDouble());
         }
-        /////////////////////////////////////////////////////////       
-        boolean loop = true;
-        while (loop) {
-            double a1 = Collections.min(listAccount);
-            listAccount.remove(a1);
-            double a2 = Collections.min(listAccount);
-            listAccount.remove(a2);
-            double result = (a1 + a2) * (100 - percent) / 100;
-            result = Double.parseDouble(String.format(Locale.US, "%.2f\n", result));            
-            listAccount.add(result);          
+        /////////////////////////////////////////////////////////      
+        while (listAccount.size() != 1) {
+            double a1 = listAccount.poll();
+            double a2 = listAccount.poll();
 
-            if (listAccount.size() == 1) {
-                loop = false;
-            }
+            double result = (a1 + a2) * (100 - percent) / 100;
+            result = Double.parseDouble(String.format(Locale.US, "%.2f\n", result));
+            listAccount.add(result);
         }
 
-        System.out.printf(Locale.US, "%.2f\n", listAccount.get(0));
+        System.out.printf(Locale.US, "%.2f\n", listAccount.poll());
     }
 
 }
